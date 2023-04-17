eac1bebee4ea03f0a31250121b6489e760b7d82b
hazlomcodes <harry.lomas1@outlook.com>
2023-04-17 14:15:24 +0100

    check win fucntion added

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def get_choices():
     choices = {"player": player_choice, "computer": computer_choice}
     return choices
 
-choices = get_choices()
-print(choices)
+def check_win(player, computer):
+  print(f"You chose {player}, computer chose {computer}")
+  if player == computer:
+    return "It's a tie!"
 
-
-food = ["pizza", "carrots", "eggs"]
-dinner= random.choice(food)
+check_win("rock","paper")
