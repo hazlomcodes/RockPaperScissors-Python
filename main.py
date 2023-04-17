c91473845d737f24aa232748d6cb124d12bf290a
hazlomcodes <harry.lomas1@outlook.com>
2023-04-17 14:04:36 +0100

    basic game options added

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-player_choice = "rock"
+import random
+
+def get_choices():
+    player_choice = input("Enter a choice (rock,paper,scissors: ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+choices = get_choices()
+print(choices)
+
+
+food = ["pizza", "carrots", "eggs"]
+dinner= random.choice(food)
